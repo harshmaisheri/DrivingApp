a6ef68ba8bf0047c5cf9ea94e41e44574ec4dfcb
harshmaisheri <harshmaisheri@gmail.com>
2020-05-17 17:12:33 +0530

    Initializing React Native App

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  // add fonts support
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Building Something AWESOME Stuffs</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
